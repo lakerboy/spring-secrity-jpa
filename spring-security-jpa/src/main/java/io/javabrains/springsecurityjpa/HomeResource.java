597080dad5cf198c44c663ac3c02f63f6988d4bf
val <valgrinberg@hotmail.com>
2019-12-09 21:18:31 -0800

    second commit

--- spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/HomeResource.java
+++ spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/HomeResource.java
@@ -3,6 +3,7 @@ package io.javabrains.springsecurityjpa;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HomeResource {
 
